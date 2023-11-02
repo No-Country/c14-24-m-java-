675f999dafddc8c574db1d8ceeb58d70eac072b4
Tatiana Gutiérrez <90115998+tatianagutierrez@users.noreply.github.com>
2023-11-02 20:27:00 -0300

    Borro credenciales y obtengo variables de entorno
    Borro datos personales del admin y, en cambio, los obtengo desde las variables de entorno que se encuentran en el deploy.

--- src/main/java/com/nocountry/c1424mjava/config/AdminUserSeeder.java
+++ src/main/java/com/nocountry/c1424mjava/config/AdminUserSeeder.java
@@ -3,6 +3,7 @@ package com.nocountry.c1424mjava.config;
 import com.nocountry.c1424mjava.model.User;
 import com.nocountry.c1424mjava.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,10 +15,16 @@ public class AdminUserSeeder implements ApplicationRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Value("${ADMIN_NAME}")
+    private String adminName;
+
+    @Value("${ADMIN_EMAIL}")
+    private String adminEmail;
+
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+
     private void seedAdmin() {
-        String adminName = "Admin";
-        String adminEmail = "admin@gmail.com";
-        String adminPassword = "123456789";
 
         if (!userRepository.findByEmail(adminEmail).isPresent()) {
             userRepository.save(new User(adminName, adminEmail, new BCryptPasswordEncoder().encode(adminPassword)));
